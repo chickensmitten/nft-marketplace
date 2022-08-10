50d34d7996fd457b34a7db48b659d08ffefa5211
sheng yeong <chia.shengyeong@gmail.com>
2022-08-10 16:00:37 +0800

    create nft page, handle links and dropdown in navbar

---
+++ pages/nft/create.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+import { BaseLayout } from "../../components";
+
+const NftCreate: NextPage = () => {
+  return (
+    <BaseLayout>
+      Create Nft
+    </BaseLayout>
+  )
+}
+
+export default NftCreate;
