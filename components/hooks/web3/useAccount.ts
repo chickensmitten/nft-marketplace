13b55826c5186acebd53bcc4e6f408540761ed90
sheng yeong <chia.shengyeong@gmail.com>
2022-08-12 10:41:55 +0800

    refactoring hooks to be more generic

--- components/hooks/web3/useAccount.ts
+++ components/hooks/web3/useAccount.ts
@@ -2,7 +2,7 @@ import { CryptoHookFactory } from "@_types/hooks";
 import useSWR from "swr";
 
 //  deps -> provider, ethereum, contract web3state
-export const hookFactory: CryptoHookFactory = (deps) => (params) => {
+export const hookFactory: CryptoHookFactory<string, string> = (deps) => (params) => {
 
   const swrRes = useSWR("web3/useAccount", () => {
     console.log(deps);
--- types/hooks.ts
+++ types/hooks.ts
@@ -8,12 +8,17 @@ export type Web3Dependencies = {
   ethereum: MetaMaskInpageProvider
 }
 
-export type CryptoSWRResponse = SWRResponse;
-
-export type CryptoHandlerHook = (params: string) => CryptoSWRResponse
-
-export type CryptoHookFactory = {
-  (d: Partial<Web3Dependencies>): CryptoHandlerHook
+export type CryptoHookFactory<D = any, P = any> = {
+  (d: Partial<Web3Dependencies>): CryptoHandlerHook<D, P>
 }
 
+export type CryptoHandlerHook<D = any, P = any> = (params: P) => CryptoSWRResponse<D>
+
+export type CryptoSWRResponse<D = any> = SWRResponse<D>;
+
+
+// another way to write the code above in a shorter manner. Code above is verbose for refactoring.
+// export type CryptoHookFactory<D = any, P = any> = {
+ //   (d: Partial<Web3Dependencies>): (params: P) => SWRResponse<D>
+ // }
 
