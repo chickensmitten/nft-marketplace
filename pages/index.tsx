d55f52d8cc77ed6948ad5018fe39f5978abd6208
sheng yeong <chia.shengyeong@gmail.com>
2022-08-12 09:54:36 +0800

    configure ganache to show contract details

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,10 +11,16 @@ const Home: NextPage = () => {
   
   const { provider, contract } = useWeb3();
 
-  console.log(contract);
+  const getNftInfo = async () => {
+    console.log(await contract!.name());
+    console.log(await contract!.symbol());
+  }
+
+  if (contract) {
+    getNftInfo();
+  }
 
   const getAccounts = async () => {
-    console.log(provider)
     const accounts = await provider!.listAccounts();
     console.log(accounts[0]);
   }
