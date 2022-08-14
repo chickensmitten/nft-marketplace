4baa50c0555829faf62c7c012d25f5f6addfa542
sheng yeong <chia.shengyeong@gmail.com>
2022-08-14 14:55:24 +0800

    create nft data type

--- types/nft.ts
+++ types/nft.ts
@@ -11,3 +11,24 @@ export type NftMeta = {
   image: string;
   attributes: NftAttribute[];
 }
+
+export type NftCore = {
+  tokenId: number;
+  price: number;
+  creator: string;
+  isListed: boolean
+}
+
+export type Nft = {
+  meta: NftMeta
+} & NftCore
+// this export creates a sub column inside NFTCore. With it, you can call
+// <constant>.meta
+//  <constant>.meta.name
+//  <constant>.meta.description
+//  <constant>.meta.image
+//  <constant>.meta.attributes
+// <constant>.tokenId
+// <constant>.price
+// <constant>.creator
+// <constant>.isListed
