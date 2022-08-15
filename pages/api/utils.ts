bfab2f1bfe39c1e8de84b2ef1d42a14185474ffa
sheng yeong <chia.shengyeong@gmail.com>
2022-08-15 21:44:13 +0800

    added ropsten to network

--- pages/api/utils.ts
+++ pages/api/utils.ts
@@ -7,7 +7,8 @@ import contract from "../../public/contracts/NftMarket.json";
 import { NftMarketContract } from "@_types/nftMarketContract";
 
 const NETWORKS = {
-  "1337": "Ganache"
+  "1337": "Ganache",
+  "3": "Ropsten"
 }
 
 type NETWORK = typeof NETWORKS;
