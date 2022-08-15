38efa5b8427a7e5f0906356f5043366d9af2d40e
sheng yeong <chia.shengyeong@gmail.com>
2022-08-15 21:06:41 +0800

    added react toastify for listing nfts and owning nfts

--- components/hooks/web3/useOwnedNfts.ts
+++ components/hooks/web3/useOwnedNfts.ts
@@ -2,6 +2,7 @@ import { CryptoHookFactory } from "@_types/hooks";
 import { Nft } from "@_types/nft";
 import { ethers } from "ethers";
 import { useCallback } from "react";
+import { toast } from "react-toastify";
 import useSWR from "swr";
 
 type UseOwnedNftsResponse = {
@@ -50,8 +51,14 @@ export const hookFactory: OwnedNftsHookFactory = ({contract}) => () => {
         }
       )
 
-      await result?.wait();
-      alert("Item has been listed!");
+      await toast.promise(
+        result!.wait(), {
+          pending: "Processing transaction",
+          success: "Item has been listed",
+          error: "Processing error"
+        }
+      );
+      
     } catch (e: any) {
       console.error(e.message);
     }
